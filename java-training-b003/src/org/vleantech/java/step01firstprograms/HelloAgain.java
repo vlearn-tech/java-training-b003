d62072111c7cf9055e9a11f9eb5ca1504f56bde8
vlearn-tech <vlearn.tech.official@gmail.com>
2021-05-29 09:03:07 +0530

    Initial Setup

---
+++ java-training-b003/src/org/vleantech/java/step01firstprograms/HelloAgain.java
@@ -0,0 +1,9 @@
+package org.vleantech.java.step01firstprograms;
+
+public class HelloAgain {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World, Again!!");
+	}
+
+}
---
+++ java-training-b003/src/org/vleantech/java/step01firstprograms/HelloWorld.java
@@ -0,0 +1,7 @@
+package org.vleantech.java.step01firstprograms;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
