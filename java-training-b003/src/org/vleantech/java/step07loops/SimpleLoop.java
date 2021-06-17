d424338902fcd69397183f384c7eaa05b1504d1c
vlearn-tech <vlearn.tech.official@gmail.com>
2021-06-17 09:47:48 +0530

    Shortcuts demo and some refactoring

--- java-training-b003/src/org/vleantech/java/step07loops/SimpleLoop.java
+++ java-training-b003/src/org/vleantech/java/step07loops/SimpleLoop.java
@@ -2,23 +2,23 @@ package org.vleantech.java.step07loops;
 
 public class SimpleLoop {
 	public static void main(String[] args) {
-		int table = 6;
+		int whichTableToPrint = 6;
 		int count = 0;
 
 		System.out.println("\n========= W H I L E   L O O P ===============");
 		while (count < 10) {
-			System.out.println(String.format("%d * %d = %d", table, ++count, table * count));
+			System.out.println(String.format("%d * %d = %d", whichTableToPrint, ++count, whichTableToPrint * count));
 		}
 
 		count = 0;
 		System.out.println("\n========= D O   W H I L E   L O O P ===============");
 		do {
-			System.out.println(String.format("%d * %d = %d", table, ++count, table * count));
+			System.out.println(String.format("%d * %d = %d", whichTableToPrint, ++count, whichTableToPrint * count));
 		} while (count < 10);
 
 		System.out.println("\n========= F O R   L O O P ===============");
 		for (int tableCount = 1; tableCount <= 20; tableCount++) {
-			System.out.println(String.format("%d * %d = %d", table, tableCount, table * tableCount));
+			System.out.println(String.format("%d * %d = %d", whichTableToPrint, tableCount, whichTableToPrint * tableCount));
 		}
 
 		// If you know a condition till which loop has to continue running - use while
---
+++ java-training-b003/src/org/vleantech/java/step99practiceprograms/shortcut/EclipseShortcutsDemo.java
@@ -0,0 +1,30 @@
+package org.vleantech.java.step99practiceprograms.shortcut;
+
+public class EclipseShortcutsDemo {
+	public static void main(String[] args) {
+
+		// Copy a line - Alt + Ctl + up/down arrow
+		// Refactor - Alt + Shift + r
+		// Delete a line - Ctrl + D
+		// Format the code - Ctrl + Shift + f
+		// Organize Imports - Ctrl + Shift + o
+		// Ctrl + space - autofill/auto suggest - most common
+		// Ctrl + 1 -- most user friendly and most powerful.
+
+		// List of all shortcuts - Ctrl + Shift + L
+
+		Person p1 = new Person();
+		p1.name = "Aaaa";
+		p1.age = 12;
+
+		Person p2 = new Person();
+		p2.name = "Aaaa";
+		p2.age = 12;
+
+	}
+}
+
+class Person {
+	String name;
+	int age;
+}
