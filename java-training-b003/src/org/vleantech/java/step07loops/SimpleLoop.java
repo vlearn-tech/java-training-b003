5261a49499ebcbf27b700e2b642f795a2a8989ce
vlearn-tech <vlearn.tech.official@gmail.com>
2021-06-19 10:21:27 +0530

    Arrays

--- java-training-b003/src/org/vleantech/java/step07loops/SimpleLoop.java
+++ java-training-b003/src/org/vleantech/java/step07loops/SimpleLoop.java
@@ -2,23 +2,26 @@ package org.vleantech.java.step07loops;
 
 public class SimpleLoop {
 	public static void main(String[] args) {
-		int whichTableToPrint = 6;
-		int count = 0;
+		int whichTableToPrint = 343;
+		int count = 1;
 
 		System.out.println("\n========= W H I L E   L O O P ===============");
-		while (count < 10) {
-			System.out.println(String.format("%d * %d = %d", whichTableToPrint, ++count, whichTableToPrint * count));
+		while (count <= 10) {
+			System.out.println(String.format("%d * %d = %d", whichTableToPrint, count, whichTableToPrint * count));
+			++count;
 		}
 
-		count = 0;
+		count = 1;
 		System.out.println("\n========= D O   W H I L E   L O O P ===============");
 		do {
-			System.out.println(String.format("%d * %d = %d", whichTableToPrint, ++count, whichTableToPrint * count));
-		} while (count < 10);
+			System.out.println(String.format("%d * %d = %d", whichTableToPrint, count, whichTableToPrint * count));
+			++count;
+		} while (count <= 10);
 
 		System.out.println("\n========= F O R   L O O P ===============");
-		for (int tableCount = 1; tableCount <= 20; tableCount++) {
-			System.out.println(String.format("%d * %d = %d", whichTableToPrint, tableCount, whichTableToPrint * tableCount));
+		for (int tableCount = 1; tableCount <= 10; tableCount++) {
+			System.out.println(
+					String.format("%d * %d = %d", whichTableToPrint, tableCount, whichTableToPrint * tableCount));
 		}
 
 		// If you know a condition till which loop has to continue running - use while
