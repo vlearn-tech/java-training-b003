488179b9c1cbeeccc1a74612ed73e04f19cce4a3
vlearn-tech <vlearn.tech.official@gmail.com>
2021-06-25 10:01:08 +0530

    Classes, Objects and Constructors

---
+++ java-training-b003/src/org/vleantech/java/step11classes/Address.java
@@ -0,0 +1,21 @@
+package org.vleantech.java.step11classes;
+
+public class Address {
+	String houseNumber;
+	String street;
+	String city;
+	Integer zip;
+
+	public Address(String houseNumber, String street, String city, Integer zip) {
+		this.houseNumber = houseNumber;
+		this.street = street;
+		this.city = city;
+		this.zip = zip;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Address [houseNumber=%s, street=%s, city=%s, zip=%s]", houseNumber, street, city, zip);
+	}
+
+}
